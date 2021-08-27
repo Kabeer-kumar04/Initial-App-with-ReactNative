1ebfa07d5b7af0ab1cebd8d29925cd231aae44d1
kabeer kumar <kabeerkumar908@gmail.com>
2021-08-27 16:05:10 +0500

    Put down info of babar

--- src/screens/Component/ImageDetail.js
+++ src/screens/Component/ImageDetail.js
@@ -1,17 +1,68 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
 
-
-const ImageDetail = () => {
+const ImageDetail = (props) => {
     return (
-        <View>
-            <Text>image detailsss</Text>
+        <View style={styles.view}>
+            <Image
+                style={styles.Image}
+                source={props.imageSource}
+            />
+            <Text style={styles.heading}>{props.heading}</Text>
+            <Text style={styles.text}>{props.text}</Text>
+            <Text style={styles.Paragraph}>{props.Paragraph}</Text>
+            <Text style={styles.Head}>{props.testHead}</Text>
+            <Text style={styles.Ranking}>{props.testRanking}</Text>
+            <Text style={styles.Head}>{props.ODIHead}</Text>
+            <Text style={styles.Ranking}>{props.ODIRanking}</Text>
+            <Text style={styles.Head}>{props.T20Head}</Text>
+            <Text style={styles.Ranking}>{props.T20Ranking}</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    view: {
+        backgroundColor: "lightgreen"
+    },
+    Image: {
+        width: 320,
+        height: 220,
+        marginHorizontal: 50,
+        marginVertical: 50,
+        borderRadius: 10
+    },
+    heading: {
+        fontSize: 40,
+        fontWeight: "bold",
+        marginHorizontal: 10,
+        marginVertical: -10,
+    },
+    text: {
+        fontSize: 20,
+        marginVertical: 5,
+        marginHorizontal: 10,
 
+    },
+    Paragraph: {
+        fontSize: 18,
+        marginVertical: 5,
+        marginHorizontal: 10,
+        textAlign: "justify"
+    },
+    Head: {
+        fontSize: 25,
+        marginVertical: 10,
+        marginHorizontal: 10,
+        textAlign: "center"
+    },
+    Ranking: {
+        fontSize: 40,
+        fontWeight: "bold",
+        marginVertical: 5,
+        marginHorizontal: 10,
+        textAlign: "center"
+    }
 })
 
 export default ImageDetail;
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, ScrollView, StyleSheet } from 'react-native';
 import ImageDetail from '../Component/ImageDetail';
-
-
 
 const ImageScreen = () => {
     return (
-        <View>
-            <ImageDetail />
-            <ImageDetail />
-            <ImageDetail />
-            <ImageDetail />
-        </View>
+        <ScrollView>
+            <ImageDetail
+                imageSource={require('../../assets/baber.jpg')}
+                heading="Babar Azam"
+                text="(The Cricket legend)"
+                Paragraph="Mohammad Babar Azam is a Pakistani international cricketer who captains Pakistan in all formats. A right-handed top-order batsman, Azam is widely regarded as one of the best contemporary batsmen in the world. He plays for Karachi Kings in Pakistan Super League and captains Central Punjab in domestic cricket."
+                testHead="Test Ranking"
+                testRanking={7}
+                ODIHead="ODI Ranking"
+                ODIRanking={1}
+                T20Head="T20 Ranking"
+                T20Ranking={2}
+            />
+        </ScrollView>
     )
 }
 
